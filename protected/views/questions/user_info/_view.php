c4b7daeeb52ed24b4aa48763fa1c1f7573e8f6df
AlexandrSafonov <alexandr.safonov.89@gmail.com>
2015-06-19 16:28:34 +0300

    safonov

---
+++ protected/views/questions/user_info/_view.php
@@ -0,0 +1,40 @@
+<div class="span3">
+    <strong class="user-title">
+        <a href="<?php echo Yii::app()->createURL('user/profile/userProfile', array('id' => $data->user_id)); ?>">
+            <?=$data->userProfile->first_name .' '. $data->userProfile->last_name ?>
+        </a>
+    </strong>
+    <p><img class="img-rounded" alt="" src="<?=$data->userProfile->imageUrl?>"></p>
+    <?php
+    $isCurrentUser = $data->user_id  == Yii::app()->user->id;
+    if (!$isCurrentUser)
+        echo '<p>
+                <a href="' . Yii::app()->createURL('messages/conversationWith', array('userId' => $data->user_id)) . '">
+                    <i class="icon-envelope" rel="tooltip" title="'.tt('Send message').'"></i>'.tt('Send message').'
+                </a>
+             </p>';
+    ?>
+    <div id="answers-block">
+        <p>
+        <?php
+       // die(var_dump($data->userAnswer));
+            if(isset($allAnswer)) {
+                foreach ($allAnswer as $answerName => $answerCount)
+                    echo '<b>'.tt($answerName).'</b></a>: '.$answerCount.'<br>';
+            }
+        ?>
+            <a href="#<?=$answerName?>" id="<?=$data->question_id?>" class="statistic <?=$answerName?>"><?=tt('Statistic')?></a>
+        </p>
+    </div>
+</div>
+
+<script>
+    $('.statistic').unbind('click').on('click',function(){
+        var questionId = $(this).attr('id');
+        $.post("/questions/getVoteStatistic",
+            { questionId: questionId },
+            function(data){
+                $('#answers-block').after(data);
+            });
+    });
+</script>
